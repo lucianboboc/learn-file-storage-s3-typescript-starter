48735eb2fbbd2ccfce672b017c5a2448ec9e786e
Lucian Boboc <info@lucianboboc.com>
2025-08-21 23:26:05 +0200

    Added Signed URLs for videos

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -4,7 +4,7 @@ import {type ApiConfig, cfg} from "../config";
 import type {BunRequest} from "bun";
 import {BadRequestError, NotFoundError, UserForbiddenError} from "./errors.ts";
 import {getBearerToken, validateJWT} from "../auth.ts";
-import {getVideo, updateVideo} from "../db/videos.ts";
+import {getVideo, updateVideo, type Video} from "../db/videos.ts";
 import {randomBytes} from "crypto";
 import * as path from "node:path";
 import {rm} from "node:fs/promises";
@@ -140,4 +140,17 @@ async function processVideoForFastStart(inputFilePath: string) {
 	}
 
 	return outputPath;
+}
+
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
+	if (!video.videoURL) {
+		return video;
+	}
+
+	video.videoURL = await generatePresignedURL(cfg, video.videoURL, 5 * 60);
+	return video;
+}
+
+async function generatePresignedURL(cfg: ApiConfig, key: string, expireTime: number) {
+	return cfg.s3Client.presign(`${key}`, {expiresIn: expireTime});
 }
